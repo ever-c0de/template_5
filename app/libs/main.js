0aef06b2aa2df0dfa166c758f109877167400e9c
ever-c0de <serhii.lavryn9nko@gmail.com>
2020-09-03 17:56:39 +0300

    continue!

--- app/libs/main.js
+++ app/libs/main.js
@@ -1,8 +1,6 @@
 (function () {
   "use strict";
 
-  /* Slider section */
-
   $(document).ready(function () {
     $(".slider").slick({
       // autoplay
@@ -11,44 +9,9 @@
       slidesToShow: 2,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 10000,
+      autoplaySpeed: 5000,
       dots: true,
       rows: 0,
     });
-
-    // /* Development slider */
-
-    // let sl = document.getElementById("mark");
-    // let bt = document.getElementsByClassName("development__block").length;
-
-    // /* Mentor's slider */
-    // $(".slider-bottom").slick({
-    //   // autoplay
-    //   arrows: false,
-    //   infinite: true,
-    //   slidesToShow: 1,
-    //   slidesToScroll: 1,
-    //   autoplay: true,
-    //   autoplaySpeed: 10000,
-    //   dots: true,
-    //   rows: 0,
-    // });
-
-    // let show_burger = document.getElementById("burger");
-    // let burger_button = document.getElementById("burger-button");
-    // show_burger.style.display = "none";
-    // burger_button.addEventListener("click", function () {
-
-    //   if (show_burger.style.display == "none") {
-    //     show_burger.style.display = "flex";
-    //   } else {
-    //     show_burger.style.display = "none";
-    //   }
-    // });
-    // window.addEventListener('resize', function () {
-    //   if (window.innerWidth >= 852) {
-    //     show_burger.style.display = "none";
-    //   }
-    // });
   });
 })();
