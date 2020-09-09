19b696e2f8e2ff168fa49f5a8ffa039de9f63a0e
ever-c0de <serhii.lavryn9nko@gmail.com>
2020-09-09 13:48:31 +0300

    cool!

--- app/libs/main.js
+++ app/libs/main.js
@@ -21,9 +21,25 @@
             infinite: true,
             dots: true,
             rows: 0,
-          }
+          },
         },
-      ]
+      ],
+    });
+
+    var show_burger = document.getElementById("burger__menu");
+    var burger_button = document.getElementById("burger-button");
+    show_burger.style.display = "none";
+    burger_button.addEventListener("click", function () {
+      if (show_burger.style.display == "none") {
+        show_burger.style.display = "flex";
+      } else {
+        show_burger.style.display = "none";
+      }
+    });
+    window.addEventListener("resize", function () {
+      if (window.innerWidth >= 852) {
+        show_burger.style.display = "none";
+      }
     });
   });
 })();
